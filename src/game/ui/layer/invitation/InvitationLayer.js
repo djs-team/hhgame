14be26fb79bb1d19d021f4c2b4579013d379c457
alvin <alvin185@163.com>
2020-09-10 19:30:14 +0800

    邀请有礼

--- src/game/ui/layer/invitation/InvitationLayer.js
+++ src/game/ui/layer/invitation/InvitationLayer.js
@@ -11,24 +11,24 @@ load('game/ui/layer/invitation/InvitationLayer', function () {
         },
         RES_BINDING: function () {
             return {
-                'pnl/returnBtn': {},
+                'pnl/returnBtn': {onClicked : this.returnClike},
                 'pnl/bgPnl': {},
 
+                'pnl/invitationPnl': {},
                 'pnl/invitationPnl/invitNowPnl': {},
                 'pnl/invitationPnl/inviteNowBtn': {onClicked : this.onInviteNowClick},
-
                 'pnl/invitationPnl/sfpPnl': {},
                 'pnl/invitationPnl/sfpPnl/sftWxBtn': {onClicked : this.onWxShareClick},
                 'pnl/invitationPnl/sfpPnl/sftWxCircleBtn': {onClicked : this.onWxCircleShareClick},
-
                 'pnl/invitationTasksPnl/myTaskList': {},
                 'pnl/invitationTasksPnl/myTaskCell': {},
 
                 'pnl/myInvitationsPnl': {},
-                'pnl/myInvitationsPnl/shareTaskTitle': {},
+                'pnl/myInvitationsPnl/dataText': {},
                 'pnl/myInvitationsPnl/shareTaskList': {},
                 'pnl/myInvitationsPnl/shareTaskCell': {},
 
+                'pnl/leftPnl': {},
                 'pnl/leftPnl/invitationBtn': {onClicked: this.inviChoiceClick},
                 'pnl/leftPnl/invitationTasksBtn': {onClicked: this.inviChoiceClick},
                 'pnl/leftPnl/myInvitationsBtn': {onClicked: this.inviChoiceClick},
@@ -41,6 +41,7 @@ load('game/ui/layer/invitation/InvitationLayer', function () {
         },
         onEnter: function () {
             this._super()
+            this.initData()
             this.initView()
         },
         onExit: function () {
@@ -52,20 +53,73 @@ load('game/ui/layer/invitation/InvitationLayer', function () {
         },
 
         initData: function () {
-            this._agentFlag = appInstance.dataManager().getUserData().agentFlag
-            this._returnType = 1
+            this._agentFlag = appInstance.dataManager().getUserData().agentFlag//0不是代理 1是1级代理 2是2级代理
+            this._returnType = 1//1是关闭当前界面，2是返回邀请好友界面
             this._inviChoiceType = 1//1代表邀请好友，2代表我的邀请，3代表分享详情
+            this._startIndex = 0//邀请记录，开始行
+            this._endIndex = 5//邀请记录，结束行
+            this._indexLength = 5//邀请记录，记录长度
+            this._isCanRefreshMyInvitationsData = true
+
+            this.myTaskList.setVisible(false)
+            this.myTaskCell.setVisible(false)
+
+            this.invitNowPnl.setVisible(false)
+            this.sfpPnl.setVisible(false)
+
+            this.invitationTasksPnl.setVisible(false)
+            this.myInvitationsPnl.setVisible(false)
+            this.dataText.setVisible(false)
+
+           // this.myTaskList.addEventListener(this.selectedItemEvent,this)
         },
+
+        onInviteNowClick : function () {
+
+            this.leftPnl.setVisible(false)
+            this.myTaskList.setVisible(false)
+            this.myTaskCell.setVisible(false)
+            this.invitNowPnl.setVisible(false)
+            this.invitationTasksPnl.setVisible(false)
+            this.myInvitationsPnl.setVisible(false)
+            this.dataText.setVisible(false)
+
+            this.sfpPnl.setVisible(true)
+        },
+
 
         /**
          * 初始化样式
          */
         initView: function () {
 
-            this.myTaskList.addEventListener(this.selectedItemEvent,this)
+            this.updateInviChoiceBtn()
 
-            this.inviChoiceClick()
+        },
 
+        returnClike: function () {
+
+            if(this._returnType == 1){
+                appInstance.uiManager().removeUI(this)
+            }else{
+                this.leftPnl.setVisible(true)
+                this.updateInviChoiceBtn()
+            }
+
+
+        },
+
+        selectedItemEvent: function (sender, type) {
+            if (type !== ccui.ListView.EVENT_SELECTED_ITEM) {
+                return
+            }
+            let curIndex = sender.getCurSelectedIndex()
+            cc.log('-----------------------curIndex---------------')
+            let offIndex = 3 //一页展示的子节点cell
+            let childLen = sender.getItems().length
+            if ( curIndex > (childLen - offIndex)) {
+                cc.log('=======滑动到底了===========')
+            }
         },
 
 
@@ -121,25 +175,207 @@ load('game/ui/layer/invitation/InvitationLayer', function () {
 
 
             if(this._inviChoiceType == 3)
-                this.updateMyInvitationsData()
+                this.showMyInvitationsData()
             else if(this._inviChoiceType == 2)
-                this.updateInvitationTasksData()
+                this.showInvitationTasksData()
             else
-                this.updateInvitationsData()
+                this.showInvitationsData()
 
         },
 
-        selectedItemEvent: function (sender, type) {
-            if (type !== ccui.ListView.EVENT_SELECTED_ITEM) {
+
+        showInvitationTasksData: function () {
+
+            appInstance.gameAgent().httpGame().myInviteReq()
+
+        },
+
+        showInvitationsData: function () {
+
+            this.invitNowPnl.setVisible(true)
+            this.myTaskList.setVisible(false)
+            this.myTaskCell.setVisible(false)
+            this.sfpPnl.setVisible(false)
+            this.invitationTasksPnl.setVisible(false)
+            this.myInvitationsPnl.setVisible(false)
+
+        },
+
+        showMyInvitationsData: function () {
+
+            this.invitNowPnl.setVisible(false)
+            this.myTaskList.setVisible(false)
+            this.myTaskCell.setVisible(false)
+            this.sfpPnl.setVisible(false)
+            this.invitationTasksPnl.setVisible(false)
+            this.myInvitationsPnl.setVisible(true)
+
+            if(this._agentFlag == 0){
+                this.dataText.setVisible(true)
                 return
             }
-            let curIndex = sender.getCurSelectedIndex()
-            cc.log('-----------------------curIndex---------------')
-            let offIndex = 3 //一页展示的子节点cell
-            let childLen = sender.getItems().length
-            if ( curIndex > (childLen - offIndex)) {
-                cc.log('=======滑动到底了===========')
+
+
+            this.refreshMyInvitationsParam(0)
+
+        },
+
+        refreshMyInvitationsData: function () {
+
+            if(!this._isCanRefreshMyInvitationsData)
+                return
+
+            let msg = {
+                'startIndex' : this._startIndex,
+                'endIndex' : this._endIndex
             }
+
+            appInstance.gameAgent().httpGame().shareInviteReq(msg)
+
+        },
+
+        refreshMyInvitationsParam: function (startIndex) {
+            this._startIndex = startIndex//邀请记录，开始行
+            this._endIndex = startIndex + this._indexLength//邀请记录，结束行
+        },
+
+
+        updateMyInvitationsData: function (data) {
+
+            if(data.length < this._indexLength){
+                this._isCanRefreshMyInvitationsData = false
+                if(this.myTaskList.getChildrenCount() <= 0){
+
+                    this.myTaskList.setVisible(false)
+                    this.dataText.setVisible(true)
+                    this.dataText.setString('当前无数据分享')
+                    return
+
+                }
+            }else{
+                if( this._startIndex == 0){
+                    this.myTaskList.removeAllChildren()
+                    this.myTaskList.setVisible(true)
+                }
+
+                this.refreshMyInvitationsParam(this._endIndex)
+            }
+
+
+            for(let i = 0; i < data.length; i++){
+
+                let log = data[i]
+                let cell = this.shareTaskCell.clone()
+                cell.setVisible(true)
+                this.myTaskList.pushBackCustomItem(cell)
+
+                cell.getChildByName('numberText').setString(log.seq)
+                cell.getChildByName('timeDaysText').setString(log.timeDays)
+                cell.getChildByName('timeHoursText').setString(log.timeHours)
+                cell.getChildByName('idText').setString(log.invitePid)
+                cell.getChildByName('nicknameText').setString(log.pName)
+
+            }
+
+        },
+
+        onUpMyInviteData: function (data) {
+
+            for(let i = 0; i < data.length; i++){
+                this.initTaskCell(data[i])
+            }
+
+        },
+
+        initTaskCell: function (dailyTask) {
+
+
+            let cell = this.myTaskCell.clone()
+            cell._data = {
+                taskId: dailyTask.taskId,
+                status: dailyTask.status
+            }
+
+            cell.setName(dailyTask.taskId)
+            cell.getChildByName('myInviteNameText').setString(dailyTask.taskName)
+            cell.getChildByName('moneyText').setString('x' + dailyTask.propNum)
+            cell.getChildByName('acceptedBg').getChildByName('moneyPic').loadTexture(dailyTask.propRes)
+            cell.getChildByName('progressBarPnl').getChildByName('progressBar').setPercent(Math.round(dailyTask.reachNum/dailyTask.taskNum*100))
+            cell.getChildByName('progressBarPnl').getChildByName('progressBarText').setString(dailyTask.reachNum + '/' + dailyTask.taskNum)
+
+
+            switch (dailyTask.status) {
+                case 0:
+                    cell.getChildByName('myInviteUnfinishedBtn').setVisible(true)
+                    cell.getChildByName('myInviteProcessingBtn').setVisible(false)
+                    cell.getChildByName('myInviteFinishedBtn').setVisible(false)
+
+                    cell.getChildByName('myInviteUnfinishedBtn').addClickEventListener(function(sender, et) {
+                        this.onTaskCellClick(sender)
+                    }.bind(this))
+                    break
+                case 1:
+                    cell.getChildByName('myInviteUnfinishedBtn').setVisible(false)
+                    cell.getChildByName('myInviteProcessingBtn').setVisible(true)
+                    cell.getChildByName('myInviteFinishedBtn').setVisible(false)
+
+                    cell.getChildByName('myInviteProcessingBtn').addClickEventListener(function(sender, et) {
+                        this.onTaskCellClick(sender)
+                    }.bind(this))
+                    break
+                case 2:
+                    cell.getChildByName('myInviteUnfinishedBtn').setVisible(false)
+                    cell.getChildByName('myInviteProcessingBtn').setVisible(false)
+                    cell.getChildByName('myInviteFinishedBtn').setVisible(true)
+                    break
+                default:
+                    cell.getChildByName('myInviteUnfinishedBtn').setVisible(true)
+                    cell.getChildByName('myInviteProcessingBtn').setVisible(false)
+                    cell.getChildByName('myInviteFinishedBtn').setVisible(false)
+
+                    cell.getChildByName('myInviteUnfinishedBtn').addClickEventListener(function(sender, et) {
+                        this.onTaskCellClick(sender)
+                    }.bind(this))
+                    break
+            }
+
+
+            cell.setVisible(true)
+            this.myTaskList.pushBackCustomItem(cell)
+
+        },
+
+        onTaskCellClick: function (sender) {
+            let data = sender.getParent()._data
+
+            let status = data.status
+            switch (status) {
+                case 0:
+                    this._inviChoiceType = 1
+                    this.updateInviChoiceBtn()
+                    break
+                case 1:
+                    //调用http方法，领取货币
+                    let msg = {
+                        taskId : data.taskId
+                    }
+                    appInstance.gameAgent().httpGame().receiveMyInviteReq(msg)
+
+                    break
+                default:
+                    break
+
+            }
+
+        },
+
+
+        updateTaskByTaskId: function (data) {
+
+            this.myTaskList.getChildByName(data.taskId).getChildByName('myInviteProcessingBtn').setVisible(false)
+            this.myTaskList.getChildByName(data.taskId).getChildByName('myInviteFinishedBtn').setVisible(false)
+            this.myTaskList.getChildByName(data.taskId)._data.status = data.status
+
         },
 
 
--- src/game/ui/layer/invitation/InvitationMdt.js
+++ src/game/ui/layer/invitation/InvitationMdt.js
@@ -6,6 +6,8 @@
 load('game/ui/layer/invitation/InvitationMdt', function () {
     let Mediator = include('public/components/Mediator')
     let GameEvent = include('game/config/GameEvent')
+    let GameUtil = include('game/public/GameUtil')
+    let GameConfig = include('game/config/GameConfig')
     let mdt = Mediator.extend({
         mediatorName: 'InvitationMdt',
         ctor: function (view) {
@@ -18,7 +20,7 @@ load('game/ui/layer/invitation/InvitationMdt', function () {
                 GameEvent.SHARE_INVITE,
             ]
         },
-        _page: 7,
+        _seq : 1,
         handleNotification: function (notification) {
             let name = notification.getName()
             let body = notification.getBody()
@@ -60,26 +62,22 @@ load('game/ui/layer/invitation/InvitationMdt', function () {
          * @param msg
          */
         receiveMyInvite: function (msg) {
+
+
+            let propData = {}
+            msg.propType = 1
+            GameUtil.getPropData(msg,propData,GameUtil.CURRENCYTYPE_1,GameUtil.UNITLOCATION_BEFORE,'x')
+
+            let propList = []
+            propList.push(propData)
+            appInstance.gameAgent().addReceivePropsUI(propList)
+
             let data = {}
             data.taskId = msg.taskId
-            data.rewards = {}
             data.status = 2
-            data.propNum = msg.propNum
-            switch (msg.propCode) {
-                case 1:
-                    data.propRes = 'res/code/task/jinbi.png'
-                    break
-                case 2:
-                    data.propRes = 'res/code/task/zuanshi.png'
-                    break
-                case 3:
-                    data.propRes = 'res/code/task/gy_fk.png'
-                    break
-                default:
-                    data.propRes = 'res/code/task/jinbi.png'
-                    break
-            }
-            this.view.onReceiveMyInviteReward(data)
+            this.view.updateTaskByTaskId(data)
+
+
         },
 
         /**
@@ -87,7 +85,7 @@ load('game/ui/layer/invitation/InvitationMdt', function () {
          * @param msg
          */
         myInviteData: function (msg) {
-            let data = {}
+            let data = []
             for (let i = 0; i < msg.taskList.length; i++) {
                 let myInviteTask = msg.taskList[i]
                 let myInviteTaskData = {}
@@ -115,25 +113,7 @@ load('game/ui/layer/invitation/InvitationMdt', function () {
                         break
                 }
 
-                //状态信息
-                switch (myInviteTask.status) {
-                    case 0:
-                        myInviteTaskData.statusRes = 'res/code/task/dqxz_3.png'
-                        myInviteTaskData.statusText = '去完成'
-                        break
-                    case 1:
-                        myInviteTaskData.statusRes = 'res/code/task/yqyl_7.png'
-                        myInviteTaskData.statusText = '未领取'
-                        break
-                    case 2:
-                        myInviteTaskData.statusRes = 'res/code/task/yj_3.png'
-                        break
-                    default:
-                        myInviteTaskData.statusRes = 'res/code/task/dqxz_3.png'
-                        myInviteTaskData.statusText = '去完成'
-                        break
-                }
-                data[i] = myInviteTaskData
+                data.push(myInviteTaskData)
             }
             this.view.onUpMyInviteData(data)
         },
@@ -143,21 +123,35 @@ load('game/ui/layer/invitation/InvitationMdt', function () {
          * @param msg
          */
         shareInviteData: function (msg) {
-            let inviteData = msg.inviteLogList
-            let data = {}
-            if (inviteData.length>0) {
-                for (let i = 0; i < inviteData.length; i++) {
-                    let info = {
-                        'id': i+1,
-                        'invitePid': inviteData[i].invitePid,
-                        'pName': inviteData[i].pName,
-                        'createTime': inviteData[i].createTime
-                    }
-                    data[i] = info
+
+            let data = []
+            for(let i = 0; i < msg.inviteLogList.length; i++){
+                let log = msg.inviteLogList[i]
+                let logData = {
+                    seq : this._seq,
+                    invitePid : log.invitePid,
+                    pName : log.pName,
                 }
+
+                this._seq++
+                this.onFormatTimes(log.pName,logData)
+                data.push(logData)
             }
-            this.view.onUpShareInviteData(data)
+
+            this.view.updateMyInvitationsData(data)
         },
+
+        onFormatTimes: function (timestamp,data) {
+
+            let date = new Date(timestamp)
+            data.timeDays = (date.getFullYear()) + "-" + (this.padLeftZero(date.getMonth() + 1)) + "-" + (this.padLeftZero(date.getDate()))
+            data.timeHours = (this.padLeftZero(date.getHours())) + "-" + (this.padLeftZero(date.getMinutes()))
+
+        },
+
+        padLeftZero: function (str) {
+            return ('00' + str).substr(str.length);
+        }
 
 
     })

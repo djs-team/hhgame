9b11f3008a01bd40c117a7561f54f60b23518097
cocos <cocoscoder@outlook.com>
2020-10-15 16:59:22 +0800

    金币商城，购买金币

--- src/game/ui/layer/coinshop/CoinShopLayer.js
+++ src/game/ui/layer/coinshop/CoinShopLayer.js
@@ -210,7 +210,7 @@ load('game/ui/layer/coinshop/CoinShopLayer', function () {
 
                 goodsCell._sendMsg = {
                     'goodsid': goodsData.id,
-                    'diamonds': goodsData.diamonds
+                    'price': goodsData.price
                 }
 
                 goodsCell.getChildByName('coinsPg').loadTexture(goodsData.res)
@@ -243,7 +243,6 @@ load('game/ui/layer/coinshop/CoinShopLayer', function () {
          */
         onBuyZuanFunction: function (s) {
             sender = s;
-            cc.log("----------------------onBuyZuanFunction" + JSON.stringify(sender))
             if (cc.sys.OS_IOS === cc.sys.os) {
                 if (AppConfig.applePayType == "Apple") {
                    let _sendData = sender._sendData
@@ -252,7 +251,6 @@ load('game/ui/layer/coinshop/CoinShopLayer', function () {
                        payType: 3
                    }
                    appInstance.gameAgent().httpGame().ZuanPaysOrderReq(msg)
-                   cc.log("----------------------onRechargeClicked")
                 } else {
                     this.PayType.setVisible(true)
                 }
@@ -317,7 +315,7 @@ load('game/ui/layer/coinshop/CoinShopLayer', function () {
 
             let msg
             let btnNameArray = {}
-            if (this._dataMsg.diamondsCnt < sender._sendMsg.diamonds) {
+            if (this._dataMsg.diamondsCnt < sender._sendMsg.price) {
 
                 msg = '您的钻石数量不足，请先领取钻石'
                 btnNameArray.MidBtnName = '确 定'

abedcf4b376d9c428f0479a1463ef8f6b1998838
dsandsan <871642996@qq.com>
2020-10-20 18:46:16 +0800

    我的邀请

--- src/game/ui/layer/invitation/InvitationLayer.js
+++ src/game/ui/layer/invitation/InvitationLayer.js
@@ -319,7 +319,7 @@ load('game/ui/layer/invitation/InvitationLayer', function () {
 
 
         updateMyInvitationsData: function (data) {
-
+            console.log('-------updateMyInvitationsData'+JSON.stringify(data));
             if (data.length < this._indexLength) {
                 this._isCanRefreshMyInvitationsData = false
                 if (this.shareTaskList.getChildrenCount() <= 0) {

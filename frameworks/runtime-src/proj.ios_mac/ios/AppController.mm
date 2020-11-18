bad440f34478c67cbc6878d4b59dc4e498c979fe
段景硕 <>
2020-11-18 16:25:05 +0800

    IOS 音乐问题

--- frameworks/runtime-src/proj.ios_mac/ios/AppController.mm
+++ frameworks/runtime-src/proj.ios_mac/ios/AppController.mm
@@ -289,6 +289,8 @@ static AppDelegate s_sharedApplication;
     /* cocos2d::Director::getInstance()->resume(); */
     
     [[UIApplication sharedApplication] setApplicationIconBadgeNumber:0];
+    
+    cocos2d::Application::getInstance()->applicationWillEnterForeground();
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
@@ -303,7 +305,7 @@ static AppDelegate s_sharedApplication;
     /*
      Called as part of  transition from the background to the inactive state: here you can undo many of the changes made on entering the background.
      */
-    cocos2d::Application::getInstance()->applicationWillEnterForeground();
+//    cocos2d::Application::getInstance()->applicationWillEnterForeground();
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
